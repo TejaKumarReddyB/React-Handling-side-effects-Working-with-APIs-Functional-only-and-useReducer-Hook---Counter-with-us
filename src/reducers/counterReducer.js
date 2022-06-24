448ef771d88055587e9a065a868c943c237ac0eb
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-06-24 14:01:16 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,4 +1,13 @@
 const counterReducer = (state,action) =>{
+  switch(action.type) {
+        case 'increment' :
+          return {count: state.count + 1}
+        case 'decrement' :
+          return {count: state.count - 1}
+        default :
+        throw new Error();
+      }
+
 
 }
 
